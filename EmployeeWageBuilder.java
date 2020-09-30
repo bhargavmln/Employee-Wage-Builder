28ada380ae529550302334dc1c0db07ab188643d
bhargavmln <bhargavmln@gmail.com>
2020-10-01 00:41:39 +0530

    UC13 added

--- EmployeeWageBuilder.java
+++ EmployeeWageBuilder.java
@@ -4,16 +4,19 @@ public class EmployeeWageBuilder implements IComputeEmpWage {
 	
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
-
+	
 	private ArrayList<CompanyEmpWage> companyEmpWageArrayList;
+	private Map<String, CompanyEmpWage> companyToEmpWageMap;
 
 	public EmployeeWageBuilder(){ 
 		companyEmpWageArrayList = new ArrayList<>();
+		companyToEmpWageMap = new HashMap<>();
 	}
 	
 	public void addCompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth){
 		CompanyEmpWage companyEmpWage = new CompanyEmpWage(company, empRatePerHour, numOfWorkingDays, maxHoursPerMonth);
 		companyEmpWageArrayList.add(companyEmpWage);
+		companyToEmpWageMap.put(company, companyEmpWage); 
 	}
 
 	public void computeEmpWage(){
@@ -25,12 +28,12 @@ public class EmployeeWageBuilder implements IComputeEmpWage {
 	}
 	
 	private int computeEmpWage(CompanyEmpWage companyEmpWage){
-	
+		
 		int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
 		
 		while(totalEmpHrs <= companyEmpWage.maxHoursPerMonth && totalWorkingDays < companyEmpWage.numOfWorkingDays){
 			totalWorkingDays++;
-		
+			
 			int empCheck = (int) Math.floor(Math.random() * 10) % 3; 
 			switch(empCheck){
 				case IS_FULL_TIME:
