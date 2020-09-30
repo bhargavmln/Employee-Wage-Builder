e91c918b6b16adef1343ab1bd934305ea42a08b1
bhargavmln <bhargavmln@gmail.com>
2020-09-30 23:02:35 +0530

    UC 10 final

--- CompanyEmpWage.java
+++ CompanyEmpWage.java
@@ -2,14 +2,14 @@ public class CompanyEmpWage{
 	
 	public final String company;
 	public final int empRatePerHour;
-	public final int noOfWorkingDays;
+	public final int numOfWorkingDays;
 	public final int maxHoursPerMonth;
 	public int totalEmpWage;
 	
-	public CompanyEmpWage(String company, int empRatePerHour, int noOfWorkingDays, int maxHoursPerMonth){
+	public CompanyEmpWage(String company, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth){
 		this.company = company;
 		this.empRatePerHour = empRatePerHour;
-		this.noOfWorkingDays = noOfWorkingDays;
+		this.numOfWorkingDays = numOfWorkingDays;
 		this.maxHoursPerMonth = maxHoursPerMonth;
 		totalEmpWage = 0;
 	}
